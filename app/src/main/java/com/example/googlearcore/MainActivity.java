7450ebee561199aee0bbde8f2f966561c78c1516
Saim <saimh355@gmail.com>
2019-12-12 17:22:07 +0500

    Google AR Core

--- app/src/main/java/com/example/googlearcore/MainActivity.java
+++ app/src/main/java/com/example/googlearcore/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.googlearcore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import com.google.ar.sceneform.Camera;
+import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+
 public class MainActivity extends AppCompatActivity {
+    private Scene scene;
+    private Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        CustomArFragment customArFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.ar_fragment);
+        scene = customArFragment.getArSceneView().getScene();
+        camera = scene.getCamera();
+
+        addBallonsToScene();
+
+    }
+
+    private void addBallonsToScene() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            ModelRenderable.builder()
+                    .setSource(this, Uri.parse("ballon.sfb"))
+                    .build()
+                    .thenAccept(modelRenderable -> {
+
+                    });
+        }
     }
 }
